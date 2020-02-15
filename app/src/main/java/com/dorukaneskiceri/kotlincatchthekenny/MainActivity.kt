8ff674592271c99ac0b17446d7fc0d7002b16ed6
DorukanE <dorukaneskiceri@gmail.com>
2020-02-15 17:26:54 +0300

    added images' status

--- app/src/main/java/com/dorukaneskiceri/kotlincatchthekenny/MainActivity.kt
+++ app/src/main/java/com/dorukaneskiceri/kotlincatchthekenny/MainActivity.kt
@@ -5,17 +5,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     var score = 0
+    val imageArray = ArrayList<ImageView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //ImageArray
+        imageArray.add(imageView)
+        imageArray.add(imageView2)
+        imageArray.add(imageView3)
+        imageArray.add(imageView4)
+        imageArray.add(imageView5)
+        imageArray.add(imageView6)
+        imageArray.add(imageView7)
+        imageArray.add(imageView8)
+        imageArray.add(imageView9)
+
+        hideImages()
 
         //CountDown Timer
 
@@ -48,5 +65,14 @@ class MainActivity : AppCompatActivity() {
     fun increaseScore(view: View){
         score++
         scoreText.text = "Score: $score"
+    }
+
+    fun hideImages(){
+        for (images in imageArray){
+            images.visibility = View.INVISIBLE
+        }
+        val random = Random()
+        val randomIndex = random.nextInt(9)
+        imageArray[randomIndex].visibility = View.VISIBLE
     }
 }
